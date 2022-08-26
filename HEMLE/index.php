7b8c27a8f6b184050db046b9d1d429ae6b576574
omeradjallala <92252979+omeradjallala@users.noreply.github.com>
2022-08-26 12:39:15 +0100

    first push

---
+++ HEMLE/index.php
@@ -0,0 +1,154 @@
+<?php
+    $p = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim blandit arcu nisl erat
+    sit urna. Eget et ut quam malesuada congue. Justo venenatis ac nisi adipiscing blandit
+    viverra tincidunt est luctus. Et quam tincidunt venenatis, commodo proin et. Tortor,
+    viverra libero non ac dui. Velit praesent turpis quam nam magna.";
+?>
+
+<!DOCTYTE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="shortcut icon" type="image/png" href="./images/tools/logo.png"/>
+        <title id="pagetitle"> HEMLE </title>
+        <link rel="stylesheet" href="./styles/navv.css"/>
+        <link rel="stylesheet" href="./styles/stl.css"/>
+    </head>
+
+    <body>
+        <nav>
+            <div id="logodiv">
+                <img src="./images/tools/title.png" alt="logo image"/>
+            </div>
+            <ul id="menu">
+                <a href="./"> <li> Acceuil </li> </a>
+                <a href=""> <li id="prest"> Prestation <img src="./images/tools/fleche.png" alt=""/> </li> </a>
+                <a href=""> <li> Nos clients </li> </a>
+                <a href=""> <li> A propos </li> </a>
+            </ul>
+            <ul id="tools">
+                <li> <img id="search" src="./images/tools/search.png" alt="search image"/> </li>
+                <li> <img src="./images/tools/world.png" alt="world image"/> </li>
+                <li> <button> Se connecter </button> </li>
+            </ul>
+        </nav>
+
+        <p id="title"> <img src="./images/tools/home.png" alt="home icon"/> Podcast </p>
+
+        <div id="body">
+
+            <header>
+                <img src="./images/tools/vidcontrols.png" alt="video controls"/>
+            </header>
+
+            <section id="sec1">
+
+                <article id="art_africa">
+                    <h1> Hemle </h1>
+                    <h2> Since 1987 </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim blandit arcu nisl
+                        erat sit urna. Eget et ut quam malesuada congue. Justo venenatis ac nisi adipiscing
+                        blandit viverra tincidunt est luctus. Et quam tincidunt venenatis, commodo proin et.
+                        Tortor, viverra libero non ac dui. Velit praesent turpis quam nam magna.
+                    </p>
+                </article>
+
+                <img id="africa" src="./images/africa.png" alt=""/>
+                    
+                <img src="./images/man.png" id="man" alt=""/>
+                    
+                <article id="art_man">
+                    <h1> Nos objectifs </h1>
+                    <img src="./images/tools/bar.png" alt="bar image"/> <br/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim blandit arcu nisl
+                        erat sit urna. Eget et ut quam malesuada congue. Justo venenatis ac nisi adipiscing
+                        blandit viverra tincidunt est luctus. Et quam tincidunt venenatis, commodo proin et.
+                        Tortor, <span>dignissim blandit</span> dui. Velit praesent turpis quam nam magna.
+                    </p> <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim blandit arcu nisl
+                        erat sit urna. Eget et ut quam malesuada congue. Justo venenatis ac nisi adipiscing
+                        blandit viverra tincidunt est luctus. Et quam tincidunt venenatis, commodo proin et.
+                        Tortor, <span>dignissim blandit</span> dui. Velit praesent turpis quam nam magna.
+                    </p>
+                </article>
+            
+            </section>
+
+            <section id="sec2"> </section>
+
+            <section id="sec3">
+
+                <img src="./images/lamp.png" id="lamp" alt=""/>
+                
+                <img src="./images/camera.png" id="cam" alt=""/>
+
+                <div id="objectifs">
+                    <article>
+                        <h1> Nos objectifs </h1>
+                        <h2> Tout pour le football africain </h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>Dignissim blandit</span>
+                            arcu nisl erat sit urna. Eget et ut quam malesuada congue. Justo venenatis ac nisi
+                            adipiscing blandit viverra tincidunt est luctus. Et quam tincidunt venenatis, commodo
+                            proin et. Tortor, viverra libero non ac dui. Velit praesent turpis quam nam magna.
+                        </p>
+                    </article>
+                    <div class="asides">
+                        <div class="asid"> <img src="./images/aside/Foot_Afrique14.png" alt=""/> </div>
+                        <div class="asid"> <img src="./images/aside/Foot_Afrique15.png" alt=""/> </div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section id="sec4">
+                
+                <div id="sec_info">
+                    <h1> Notre equipe de direction </h1>
+                    <div class="img"> <img src="./images/tools/bar.png" alt=""/> </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim blandit arcu nisl
+                        erat sit urna. Eget et ut quam malesuada congue. 
+                    </p>
+                
+                </div> <div id="allasid">
+
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique1.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique2.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique3.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique4.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique5.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique6.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique7.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique8.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique9.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique10.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique11.png" alt=""/> </div>
+                    <div class="asid"> <img src="./images/aside/Foot_Afrique12.png" alt=""/> </div>
+
+                </div> <div id="sec_info">
+
+                    <h1> Nos partenaires </h1>
+                    <div class="img"> <img src="./images/tools/bar.png" alt=""/> </div>
+                    <p>
+                        Nous sommes prêts à tout pour vous offir le meilleur du football africain avec :
+                    </p>
+
+                </div>
+
+            </section>
+        
+        </div>
+            
+        <footer> <?php
+            $imgs = ["ai", "slack", "trello", "google", "microsoft A", "gmail", "jira software",
+                "p", "chrome", "azure"];
+            foreach ($imgs as $img) echo "<img src='./images/footer/$img.png' alt=''/>";
+        ?> </footer>
+
+    </body>
+
+</html>
